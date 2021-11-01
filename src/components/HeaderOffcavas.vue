c494fc9f168ebd043a9bdb60018f5951152c3b33
Vasiliev Mikhail <mgp-nsk@yandex.ru>
2021-11-01 22:58:57 +0700

    with BootsttrapVue Sidebar

--- src/components/HeaderOffcavas.vue
+++ src/components/HeaderOffcavas.vue
@@ -3,12 +3,16 @@
         <div class="header__wrapper container">
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid"><a class="logo logo_left navbar-brand" href="https://yandex.ru"><img
-                            src="@/assets/default.jpg" alt="" class="logo__image">Агрегатор мер поддержки</a>
+                    <div class="container-fluid">
+                        <a class="logo logo_left navbar-brand" href="https://yandex.ru">
+                            <img src="@/assets/default.jpg" alt="" class="logo__image">
+                            Агрегатор мер поддержки
+                        </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation"><span
-                                class="navbar-toggler-icon"></span></button>
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
                         <div class="d-flex gap-2">
                             <button class="Button btn btn-outline-secondary Button_icon-only-square d-lg-none"
                                     type="button" data-bs-toggle="offcanvas" data-bs-target="#sideBar"
@@ -66,6 +70,41 @@
                     </div>
                 </nav>
             </div>
+
+            <hr>
+
+            <div>
+                <b-navbar toggleable="lg" type="dark" variant="info">
+                    <b-navbar-brand href="#">
+                        <img src="@/assets/default.jpg" class="d-inline-block align-top logo__image" alt="logo">
+                        Навигатор мер поддержки
+                    </b-navbar-brand>
+                    <div class="d-flex gap-2">
+                        <b-button v-b-toggle.sidebar>
+                            <b-icon icon="list" aria-hidden="true"></b-icon>
+                        </b-button>
+
+                        <b-sidebar id="sidebar" title="Меню" shadow>
+                            <b-nav vertical>
+                                <b-nav-item active>Программы</b-nav-item>
+                                <b-nav-item href="#link-1">Новости</b-nav-item>
+                                <b-nav-item-dropdown text="Меры поддержки" right>
+                                    <b-dropdown-item href="#">Все меры поддержки</b-dropdown-item>
+                                    <b-dropdown-item href="#">Юридическим лицам</b-dropdown-item>
+                                    <b-dropdown-item href="#">Индивидуальным предпринимателям</b-dropdown-item>
+                                    <b-dropdown-item href="#">Физическим лицам</b-dropdown-item>
+                                </b-nav-item-dropdown>
+                            </b-nav>
+                        </b-sidebar>
+                        <b-nav-form>
+                            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Поиск</b-button>
+                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Вход</b-button>
+                        </b-nav-form>
+
+                    </div>
+                </b-navbar>
+            </div>
         </div>
     </header>
 </template>
@@ -95,7 +134,7 @@
             @media (min-width: 992px) {
                 transition: transform 0s;
                 position: static;
-                visibility: visible!important;
+                visibility: visible !important;
                 flex-direction: row;
                 width: auto;
                 transform: none;
